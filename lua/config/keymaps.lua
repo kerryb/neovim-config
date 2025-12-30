c6f97cafa055b2f2cdf371f2450de199ff5eb2eb
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-30 13:18:50 +0000

    Add keymaps to copy file path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- " Copy the current file path with <leader>c
+-- nnoremap <leader>c :let @+ = expand("%")<cr>
+-- " <leader>C for <filename>:<current line>
+-- nnoremap <leader>C :let @+ = expand("%") . ":" . line(".")<cr>
+
+local map = vim.keymap.set
+
+map("n", "<leader>c", '<cmd>let @+ = expand("%")<cr>', { desc = "Copy the current file path" })
+map(
+  "n",
+  "<leader>C",
+  '<cmd>let @+ = expand("%") . ":" . line(".")<cr>',
+  { desc = "Copy the current file path and line no" }
+)
