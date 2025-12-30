8af30db734282739a17f3ca5dd001e7a6193a646
Kerry Buckley <kerryjbuckley@gmail.com>
2025-12-30 13:18:38 +0000

    Replace custom heuristics with projectionist-elixir

--- lua/plugins/projectionist.lua
+++ lua/plugins/projectionist.lua
@@ -4,40 +4,8 @@ return {
     keys = {
       { "<leader>a", "<cmd>A<cr>", desc = "Go to alternate file" },
     },
-    config = function()
-      vim.g.projectionist_heuristics = {
-        ["mix.exs"] = {
-          ["lib/*.ex"] = {
-            type = "src",
-            alternate = "test/{}_test.exs",
-            template = {
-              "# lib/{}.ex",
-              "defmodule XXX do",
-              "end",
-            },
-          },
-          ["test/*_test.exs"] = {
-            type = "test",
-            alternate = "lib/{}.ex",
-            template = {
-              "# test/{}_test.exs",
-              "defmodule XXX do",
-              "",
-              "  use ExUnit.Case",
-              "",
-              "  alias XXX",
-              "",
-              '  describe "#myfunc/1" do',
-              '    test "description" do',
-              "      assert true",
-              "    end",
-              "  end",
-              "end",
-            },
-          },
-        },
-      }
-    end,
     event = "BufEnter",
   },
+  { "c-brenn/fuzzy-projectionist.vim" },
+  { "andyl/vim-projectionist-elixir" },
 }
