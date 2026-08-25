abbb97c0380c0c5c403b76f5bc3aa69deb6aee31
Kerry Buckley <kerryjbuckley@gmail.com>
2026-08-25 13:24:30 +0100

    Another attempt to disable elixir-ls

--- lua/plugins/mason.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mason-org/mason.nvim",
-  opts = function(_, opts)
-    opts.ensure_installed = vim.tbl_filter(function(name)
-      return not vim.tbl_contains({ "elixir-ls" }, name)
-    end, opts.ensure_installed)
-  end,
-}
